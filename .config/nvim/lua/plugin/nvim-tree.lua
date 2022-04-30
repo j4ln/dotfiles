67dedcba258a71c03f46b55b293bbdde87cdae4b
James <j4ln@posteo.net>
2022-04-30 13:54:36 +0100

    feat(neovim): set nvim-tree icons

--- .config/nvim/lua/plugin/nvim-tree.lua
+++ .config/nvim/lua/plugin/nvim-tree.lua
@@ -1,3 +1,24 @@
+vim.g.nvim_tree_icons = {
+	default = "",
+	symlink = "",
+	git = {
+		unstaged = "",
+		staged = "S",
+		unmerged = "",
+		renamed = "➜",
+		deleted = "",
+		untracked = "U",
+		ignored = "◌",
+	},
+	folder = {
+		default = "",
+		open = "",
+		empty = "",
+		empty_open = "",
+		symlink = "",
+	},
+}
+
 local status_ok, nvim_tree = pcall(require, 'nvim-tree')
 if not status_ok then
 	return
